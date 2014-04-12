2556b87d4dfb4e26e19609e5e05575ac01f654ad
Will Jackson <wjackson@573680.no-reply.drupal.org>
2014-04-12 17:13:41 -0400

    Initial Commit

---
+++ resources/jtest.php
@@ -0,0 +1,16 @@
+[{
+	"id": "",
+	"int_designator":"",
+	"name":"",
+	"status":"0",
+	"period":"0",
+	"prn":"",
+	"ipos": [],
+	"mrk": "",
+	"orb": "",
+	"foot": "",
+	"sel": "0",
+	"pos": [
+	
+	]
+	}]
